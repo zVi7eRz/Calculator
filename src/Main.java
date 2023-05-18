d2f01c0eb9e8863829a82e27b5e6be9822ec6420
zVi7eRz <vi7er.7@gmail.com>
2023-05-18 09:01:38 +0300

    reduction of repetitions

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scan = new Scanner(System.in);
-
         String calc = scan.nextLine();
         scan.close();
-        String [] strings = calc.split(" ");
+
+        String [] strings = calc.split(" "); // scan = "2 + 4" : string = ["2","+","4"]
 
         if (strings.length != 3) {
-            throw new Exception();
+            throw new Exception(); // выдаст ошибку, если количество символов больше 3
         }
-        String sign = strings[1];
+        String sign = strings[1], result = "";
         String[] romanNum = {
             "N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII",
             "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV",
@@ -23,17 +22,11 @@ public class Main {
             "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
             "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
             "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
-            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
-        };
+            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C" };
+        String[] numbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
 
-        String[] numbers = {
-            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
-        };
-        String result = "";
-
-        if (lineSearch( romanNum, strings[0]) != -1 & lineSearch( romanNum, strings[2]) != -1) {
-            int num1 = lineSearch( romanNum, strings[0]); 
-            int num2 = lineSearch( romanNum, strings[2]);
+        if (lineSearch( romanNum, strings[0]) != -1 & lineSearch( romanNum, strings[2]) != -1) { // проверяем являются ли цифры римскими
+            int num1 = lineSearch( romanNum, strings[0]), num2 = lineSearch( romanNum, strings[2]);
             if (num1 > 10 | num2 > 10){
                 throw new Exception(); // Исключение больших рим. чисел
             }
@@ -63,8 +56,7 @@ public class Main {
             }
         }
         else if(lineSearch( numbers, strings[0]) != -1 & lineSearch( numbers, strings[2]) != -1) {
-            int num1 = Integer.parseInt(strings[0]);
-            int num2 = Integer.parseInt(strings[2]);
+            int num1 = Integer.parseInt(strings[0]), num2 = Integer.parseInt(strings[2]);
             switch(sign){
 
                 case "+":
@@ -92,11 +84,9 @@ public class Main {
             throw new Exception();
         }
 
-
         System.out.println(result);
 }
-    // проверка нахождения элемента в списке, возвращает index элемента в списке или -1 если его нет.
-    public static int lineSearch(String arr[], String elementToSearch) {
+    public static int lineSearch(String arr[], String elementToSearch) { // проверка нахождения элемента в списке, возвращает index элемента в списке или -1 если его нет.
         for (int i = 0; i < arr.length; ++i) {
             if (arr[i].equals(elementToSearch))
                 return i;
