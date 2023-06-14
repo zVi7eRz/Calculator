b6406651f31717b26a112a186455947f255fcae7
zVi7eRz <61157468+zVi7eRz@users.noreply.github.com>
2023-06-14 16:25:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,96 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        List<String> romanList = Arrays.asList("N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C");
 
-        Scanner scan = new Scanner(System.in);
-        String calc = scan.nextLine();
-        scan.close();
-
-        String [] strings = calc.split(" "); // scan = "2 + 4" : string = ["2","+","4"]
-
-        if (strings.length != 3) {
-            throw new Exception(); // выдаст ошибку, если количество символов больше 3
+        String[] str;
+        if ((str = (new Scanner(System.in)).nextLine().split(" ")).length != 3) throw new Exception();
+        if (new Scanner(str[0]).hasNextInt() && new Scanner(str[2]).hasNextInt()) {
+            System.out.println(calc(Integer.parseInt(str[0]), Integer.parseInt(str[2]), str[1]));
         }
-        String sign = strings[1], result = "";
-        String[] romanNum = {
-            "N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII",
-            "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV",
-            "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV",
-            "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI",
-            "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII",
-            "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
-            "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
-            "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
-            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C" };
-        String[] numbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-
-        if (lineSearch( romanNum, strings[0]) != -1 & lineSearch( romanNum, strings[2]) != -1) { // проверяем являются ли цифры римскими
-            int num1 = lineSearch( romanNum, strings[0]), num2 = lineSearch( romanNum, strings[2]);
-            if (num1 > 10 | num2 > 10){
-                throw new Exception(); // Исключение больших рим. чисел
-            }
-            switch(sign){
-
-                case "+":
-                    result = romanNum[(num1 + num2)];
-                    break;
-
-                case "-":
-                    if(num1 < num2){
-                        throw new Exception(); // Исключение отрицательного ответа среди римских чисел
-                    }
-                    result = romanNum[(num1 - num2)];
-                    break;
-
-                case "*":
-                    result = romanNum[(num1 * num2)];
-                    break;
-
-                case "/":
-                    result = romanNum[(num1 / num2)];
-                    break;
-
-                default:
-                    throw new Exception(); // Если нет нужного знака
-            }
+        if (romanList.contains(str[0]) && romanList.contains(str[2])) {
+            System.out.println(romanList.get(calc(romanList.indexOf(str[0]), romanList.indexOf(str[2]), str[1])));
         }
-        else if(lineSearch( numbers, strings[0]) != -1 & lineSearch( numbers, strings[2]) != -1) {
-            int num1 = Integer.parseInt(strings[0]), num2 = Integer.parseInt(strings[2]);
-            switch(sign){
+    }
 
-                case "+":
-                    result = Integer.toString(num1 + num2);
-                    break;
-
-                case "-":
-                    result = Integer.toString(num1 - num2);
-                    break;
-
-                case "*":
-                    result = Integer.toString(num1 * num2);
-                    break;
-                    
-                case "/":
-                    if (num2 == 0) {
-                        throw new Exception(); // на 0 делить нельзя
-                    }
-                    result = Integer.toString(num1 / num2);
-                    break;
-                default:
-                    throw new Exception(); // Если нет нужного знака
-            }
-        } else {
-            throw new Exception();
-        }
-
-        System.out.println(result);
-}
-    public static int lineSearch(String arr[], String elementToSearch) { // проверка нахождения элемента в списке, возвращает index элемента в списке или -1 если его нет.
-        for (int i = 0; i < arr.length; ++i) {
-            if (arr[i].equals(elementToSearch))
-                return i;
-        }
-        return -1;
+    public static int calc(int num1, int num2, String oper) throws Exception {
+        if ((0 >= num1 || num1 > 10) || (0 >= num2 || num2 > 10)) throw new Exception();
+        return switch (oper) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "/" -> num1 / num2;
+            case "*" -> num1 * num2;
+            default -> throw new Exception();
+        };
     }
 }
